04d2e35e260dcaecabb6696901765f760b2e0433
guocaoyi <guocaoyi@gmail.com>
2018-09-14 10:52:11 +0800

    feature(complate problem):two sum && and complate md

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Environment:
   Node:8.0+
   Java:1.8+
 
-Problems:
-1. Two Sum
+Num | Problems | With in Language  
+------------ | ------------- | ------------ 
+1 | Two Sum  | [JS](/js/src/TowSum.js) [JAVA](/java/src/TowSum.java)
+2 | Add Tow Numbers | [JS](/js/src/AddTwoNumbers.js)
--- js/src/TowSum.js
+++ js/src/TowSum.js
@@ -1,15 +1,70 @@
 /**
- * 方案一：双重遍历
+ * 方案一：两次列表遍历
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] == target) {
-        return [i, j]
-      }
+const twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j]
+            }
+        }
     }
-  }
+};
+
+/**
+* 方案二：两次哈希遍历
+* @param {number[]} nums
+* @param {number} target
+* @return {number[]}
+*/
+const twoSum = function(nums, target) {
+    const map = {};
+    nums.forEach((i, k) => map[i] = k);
+    for (let i = 0; i < nums.length; i++) {
+        const x = target - nums[i];
+        if (x in map && map[x] != i) {
+            return [i, map[x]]
+        }
+    }
+};
+
+/**
+* 方案二：两次哈希遍历(优化)
+* @param {number[]} nums
+* @param {number} target
+* @return {number[]}
+*/
+const twoSum = function(nums, target) {
+    const map = {};
+    const length = nums.length;
+    for (let i = 0; i < length; i++) {
+        map[nums[i]] = i;
+    }
+    for (let i = 0; i < length; i++) {
+        const x = target - nums[i];
+        if (x in map && map[x] != i) {
+            return [i, map[x]]
+        }
+    }
+};
+
+/**
+ * 方案三：一次哈希遍历(优化)
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]} 
+ */
+const twoSum = function(nums, target) {
+    const map = {};
+    const length = nums.length;
+    for (let i = 0; i < length; i++) {
+        const x = target - nums[i];
+        if (x in map && map[x] != i) {
+            return [map[x], i]
+        }
+        map[nums[i]] = i;
+    }
 };
