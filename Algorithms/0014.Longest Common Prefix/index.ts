07cf29a9ac2641cd665d0d4cc1feb441d39f6f7a
guocaoyi <guocaoyi@gmail.com>
2019-03-29 10:25:32 +0800

    test(test custom pages)

--- Algorithms/0014.Longest Common Prefix/index.ts
+++ Algorithms/0014.Longest Common Prefix/index.ts
@@ -1,15 +1,15 @@
-/**
- * @description Write a function to find the longest common prefix string amongst an array of strings.If there is no common prefix, return an empty string "".
- * @topics String
- * @example ["flower","flow","flight"] -> "fl"
- * @example ["dog","racecar","car"] -> ""
- * @example [1,1,2] -> [[1,1,2],[1,2,1],[2,1,1]]
- * @note All given inputs are in lowercase letters a-z.
- * @thinking
- */
 declare let longestCommonPrefix: any;
+interface Problems {
+  description: string;
+  topics: string | string[];
+  example: string | string[];
+  note?: string;
+  thinking?: string;
+}
 
-let a = {
+const problems: Problems = {
+  description: `Write a function to find the longest common prefix string amongst an array of strings.If there is no common prefix, return an empty string ""`,
+  topics: "string",
   example: `
   Input: [[1,1],[2,2],[3,3]]
   Output: 3
@@ -21,12 +21,13 @@ let a = {
   |  o  
   +------------->
   0  1  2  3  4
-  `
+  `,
+  note: `All given inputs are in lowercase letters a-z.`  
 };
 
 /**
  * 使用 `strs[0]` 作为初始前缀串，逐一遍历 `strs[]` 元素进行比较，如 `String.indexOf !== 0` 则自减长度 1，直至成立后继续访问后面的元素。
- * @submissions
+ * @submissionscjr
  * @status Accepted
  * @runtime 60ms
  * @runtime_distribution >92.74%
@@ -34,7 +35,7 @@ let a = {
  * @memory_distribution <86.74%
  * @commit
  */
-longestCommonPrefix = function(strs: Array<string>) {
+longestCommonPrefix = (strs: Array<string>) => {
   let prefix: string = "";
   if (!strs || strs.length <= 0) return "";
   let point: number = 1;
@@ -50,9 +51,9 @@ longestCommonPrefix = function(strs: Array<string>) {
 };
 
 /**
- *
+ * @param
  */
-longestCommonPrefix = function(strs: Array<string>) {
+longestCommonPrefix = (strs: Array<string>) => {
   if (!strs || strs.length <= 0) return "";
   if (strs && strs.length > 0) {
     let prefix: string = strs[0]; // 使用 strs[0] 作为初始前缀串
