50c136c28d89c506299573e00d29ee77be0f79df
guocaoyi <guocaoyi@gmail.com>
2020-09-08 10:49:40 +0800

    feat(567):add

---
+++ algorithms/0046.permutations/index.ts
@@ -0,0 +1,37 @@
+/**
+ * # 46. Permutations
+ *
+ * Given a collection of **distinct** integers, return all possible permutations.
+ *
+ * ## Example
+ *
+ * ```bash
+ * Input: [1,2,3]
+ * Output:
+ * [
+ *   [1,2,3],
+ *   [1,3,2],
+ *   [2,1,3],
+ *   [2,3,1],
+ *   [3,1,2],
+ *   [3,2,1],
+ * ]
+ * ```
+ */
+export type Solution = (nums: number[]) => number[][];
+
+/**
+ * @date
+ * @time
+ * @space
+ */
+export const permute = (nums: number[]): number[][] => {
+  const result: number[][] = [];
+  // 1 -> n
+  // 
+  const loop = () => {
+    // 回溯
+  }
+
+  return result;
+};
---
+++ algorithms/0567.permutation-in-string/index.ts
@@ -0,0 +1,32 @@
+/**
+ * # 567. Permutation in String
+ * 
+ * Given two strings *s1* and *s2*, write a function to return true if *s2* contains the permutation of *s1*. In other words, one of the first string's permutations is the *substring* of the second string.
+ * 
+ * ## Example
+ * ```typescript
+ * Input: s1 = "ab" s2 = "eidbaooo"
+ * Output: True
+ * Explanation: s2 contains one permutation of s1 ("ba").
+ * ```
+ * ```typescript
+ * Input:s1= "ab" s2 = "eidboaoo"
+ * Output: False
+ * ```
+ * 
+ * ## Constraints
+ * - The input strings only contain lower case letters.
+ * - The length of both given strings is in range [1, 10,000].
+ */
+export type Solution = (s1: string, s2: string) => boolean
+
+/**
+ * @time
+ * @space
+ */
+export const checkInclusion = (s1: string, s2: string): boolean => {
+  return false
+};
+
+// 排列组合所有字符串组合；然后查找
+// 
